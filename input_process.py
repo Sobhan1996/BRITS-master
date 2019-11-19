fcd7a7703b1bb3b6d11ddbfaa05d34853fa841d5
Sobhan1996 <mniknami@uwaterloo.ca>
2019-11-18 20:25:18 -0500

    Wrote subClass for PhysioNet dataset

--- input_process.py
+++ input_process.py
@@ -8,17 +8,87 @@ import ujson as json
 
 class BRITSDataset:
     def __init__(self):
+        self.ids = []
+        self.set_ids()
+
+    def set_ids(self):
         pass
 
-    def read_data(self):
+    def read_data(self, id_):
         pass
 
-    def set_time_column(self):
+    def get_label(self, id_):
         pass
 
-    def set_label(self):
-        pass
 
+class PhysioNetDataset(BRITSDataset):
+    def __init__(self):
+        BRITSDataset.__init__(self)
+        self.attributes = ['DiasABP', 'HR', 'Na', 'Lactate', 'NIDiasABP', 'PaO2', 'WBC', 'pH', 'Albumin', 'ALT', 'Glucose', 'SaO2',
+                           'Temp', 'AST', 'Bilirubin', 'HCO3', 'BUN', 'RespRate', 'Mg', 'HCT', 'SysABP', 'FiO2', 'K', 'GCS',
+                           'Cholesterol', 'NISysABP', 'TroponinT', 'MAP', 'TroponinI', 'PaCO2', 'Platelets', 'Urine', 'NIMAP',
+                           'Creatinine', 'ALP']
+
+        self.mean = np.array([59.540976152469405, 86.72320413227443, 139.06972964987443, 2.8797765291788986, 58.13833409690321,
+                              147.4835678885565, 12.670222585415166, 7.490957887101613, 2.922874149659863, 394.8899400819931,
+                              141.4867570064675, 96.66380228136883, 37.07362841054398, 505.5576196473552, 2.906465787821709,
+                              23.118951553526724, 27.413004968675743, 19.64795551193981, 2.0277491155660416, 30.692432164676188,
+                              119.60137167841977, 0.5404785381886381, 4.135790642787733, 11.407767149315339, 156.51746031746032,
+                              119.15012244292181, 1.2004983498349853, 80.20321011673151, 7.127188940092161, 40.39875518672199,
+                              191.05877024038804, 116.1171573535279, 77.08923183026529, 1.5052390166989214, 116.77122488658458])
+
+        self.std = np.array(
+            [13.01436781437145, 17.789923096504985, 5.185595006246348, 2.5287518090506755, 15.06074282896952, 85.96290370390257,
+             7.649058756791069, 8.384743923130074, 0.6515057685658769, 1201.033856726966, 67.62249645388543, 3.294112002091972,
+             1.5604879744921516, 1515.362517984297, 5.902070316876287, 4.707600932877377, 23.403743427107095, 5.50914416318306,
+             0.4220051299992514, 5.002058959758486, 23.730556355204214, 0.18634432509312762, 0.706337033602292,
+             3.967579823394297, 45.99491531484596, 21.97610723063014, 2.716532297586456, 16.232515568438338, 9.754483687298688,
+             9.062327978713556, 106.50939503021543, 170.65318497610315, 14.856134327604906, 1.6369529387005546,
+             133.96778334724377])
+
+        self.time_range = 48
+
+        self.fs = open('./json/json', 'w')
+
+        self.label_df = pd.read_csv('./raw/Outcomes-a.txt').set_index('RecordID')['In-hospital_death']
+
+    def set_ids(self):
+        for file_name in os.listdir('./raw'):
+            # the patient data in PhysioNet contains 6-digits
+            matched = re.search('\d{6}', file_name)
+            if matched:
+                file_id = matched.group()
+                self.ids.append(file_id)
+
+    def get_label(self, id_):
+        return self.label_df.loc[int(id_)]
+
+    def read_data(self, id_):
+
+        def to_time_bin(x):
+            h, m = map(int, x.split(':'))
+            return h
+
+        def parse_data(x):
+            x = x.set_index('Parameter').to_dict()['Value']
+
+            values = []
+
+            for attr in attributes:
+                if x.has_key(attr):
+                    values.append(x[attr])
+                else:
+                    values.append(np.nan)
+            return values
+
+        data = pd.read_csv('./raw/{}.txt'.format(id_))
+        data['Time'] = data['Time'].apply(lambda x: to_time_bin(x))
+        evals = []
+        for h in range(48):
+            evals.append(parse_data(data[data['Time'] == h]))
+        evals = (np.array(evals) - mean) / std
+
+        return evals
 
 
 patient_ids = []
@@ -116,7 +186,6 @@ def parse_id(id_):
     # accumulate the records within one hour
     data['Time'] = data['Time'].apply(lambda x: to_time_bin(x))     # 2 removing minutes from time column
 
-    print(data)
     evals = []
 
     # merge all the metrics within one hour
